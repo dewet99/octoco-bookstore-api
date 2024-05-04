23be1812db37a9d12b05a3120d110a2172b998bb
DeWet <dewet99@github.com>
2024-05-04 09:06:47 +0200

    Added functionality to display whether any books are in the genre that the total discounted price is calculated of

--- src/routes/discounted-price.ts
+++ src/routes/discounted-price.ts
@@ -9,8 +9,6 @@ const router = Router();
 router.get('/', (req: Request, res: Response) => {
     const genre = req.query.genre as string;
     const discountPercentage = parseFloat(req.query.discount as string);
-    
-    // debug
 
     // Validate query parameters
     if (!genre || isNaN(discountPercentage) || discountPercentage<0 || discountPercentage>100) {
@@ -19,6 +17,11 @@ router.get('/', (req: Request, res: Response) => {
 
     // Filter books by the specified genre:
     const genreBooks = getBooks(genre);
+
+    if (!genreBooks){
+        return res.status(404).json({error: "No books of that genre"});
+    }
+
     // Calculate total price and discounted price
     const totalPrice = genreBooks.reduce((total, book) => total + book.price, 0);
     const total_discounted_price = totalPrice * (1-discountPercentage/100);
